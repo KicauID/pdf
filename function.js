3bdd410ce4bad88572023f29cbc9f6f9d5dfef62
KicauID <Kicauid.official@gmail.com>
2024-07-01 10:18:55 +0700

    Update function.js

--- function.js
+++ function.js
@@ -1,8 +1,12 @@
+// Initialize html2pdf before using it
+const html2pdf = require('html2pdf.js'); // or include the script in your HTML file
+
+// Modify your window.function to include html2pdf usage
 window.function = function (html, fileName, format, zoom, orientation, margin, breakBefore, breakAfter, breakAvoid, fidelity, customDimensions) {
     // DYNAMIC VALUES
     html = html.value ?? "No HTML set.";
     fileName = fileName.value ?? "file";
-    format = format.value ?? "thermal_80mm";
+    format = format.value ?? "thermal_80mm"; // Ubah format default sesuai kebutuhan, contoh thermal_80mm
     zoom = zoom.value ?? "1";
     orientation = orientation.value ?? "portrait";
     margin = margin.value ?? "0";
@@ -11,10 +15,51 @@ window.function = function (html, fileName, format, zoom, orientation, margin, b
     breakAvoid = breakAvoid.value ? breakAvoid.value.split(",") : [];
     customDimensions = customDimensions.value ? customDimensions.value.split(",").map(Number) : null;
 
-    // DOCUMENT DIMENSIONS HANYA UNTUK A4 DAN THERMAL 80MM
+    // DOCUMENT DIMENSIONS
     const formatDimensions = {
-        a4: [210, 297], // Format A4 dalam mm (width, height)
-        thermal_80mm: [80, 225] // Format thermal 80mm dalam mm (width, height)
+        a0: [4967, 7022],
+        a1: [3508, 4967],
+        a2: [2480, 3508],
+        a3: [1754, 2480],
+        a4: [1240, 1754],
+        a5: [874, 1240],
+        a6: [620, 874],
+        a7: [437, 620],
+        a8: [307, 437],
+        a9: [219, 307],
+        a10: [154, 219],
+        b0: [5906, 8350],
+        b1: [4175, 5906],
+        b2: [2953, 4175],
+        b3: [2085, 2953],
+        b4: [1476, 2085],
+        b5: [1039, 1476],
+        b6: [738, 1039],
+        b7: [520, 738],
+        b8: [366, 520],
+        b9: [260, 366],
+        b10: [183, 260],
+        c0: [5415, 7659],
+        c1: [3827, 5415],
+        c2: [2705, 3827],
+        c3: [1913, 2705],
+        c4: [1352, 1913],
+        c5: [957, 1352],
+        c6: [673, 957],
+        c7: [478, 673],
+        c8: [337, 478],
+        c9: [236, 337],
+        c10: [165, 236],
+        dl: [650, 1299],
+        letter: [1276, 1648],
+        government_letter: [1199, 1577],
+        legal: [1276, 2102],
+        junior_legal: [1199, 750],
+        ledger: [2551, 1648],
+        tabloid: [1648, 2551],
+        credit_card: [319, 508],
+        thermal_58mm: [154, 1050],
+        thermal_80mm: [224, 1050],
     };
 
     // GET FINAL DIMENSIONS FROM SELECTED FORMAT
@@ -59,26 +104,6 @@ window.function = function (html, fileName, format, zoom, orientation, margin, b
         z-index: 1000;
     }
 
-    button#download {
-        background: #04A535;
-        left: 0;
-    }
-
-    button#download:hover {
-        background: #f5f5f5;
-        color: #000000;
-    }
-
-    button#download.downloading {
-        background: #ffffff;
-        color: #000000;
-    }
-
-    button#download.done {
-        background: #ffffff;
-        color: #000000;
-    }
-
     button#print {
         background: #0353A7;
         right: 0;
@@ -113,40 +138,10 @@ window.function = function (html, fileName, format, zoom, orientation, margin, b
     const originalHTML = `
     <style>${customCSS}</style>
     <div class="main">
-        <button class="button" id="download">Download</button>
         <button class="button" id="print">Print</button>
         <div id="content" class="content">${html}</div>
     </div>
     <script>
-        document.getElementById('download').addEventListener('click', function() {
-            var element = document.getElementById('content');
-            var button = this;
-            button.innerText = 'DOWNLOADING...';
-            button.className = 'downloading';
-
-            var opt = {
-                margin: ${margin},
-                filename: '${fileName}',
-                html2canvas: {
-                    useCORS: true,
-                    scale: 2 // Skala rendering untuk meningkatkan kualitas gambar jika diperlukan
-                },
-                jsPDF: {
-                    unit: 'in',
-                    orientation: '${orientation}',
-                    format: [${finalDimensions}]
-                }
-            };
-            html2pdf().set(opt).from(element).toPdf().get('pdf').then(function(pdf) {
-                button.innerText = 'DOWNLOAD DONE';
-                button.className = 'done';
-                setTimeout(function() { 
-                    button.innerText = 'Download';
-                    button.className = ''; 
-                }, 2000);
-            }).save();
-        });
-
         document.getElementById('print').addEventListener('click', function() {
             var element = document.getElementById('content');
             var button = this;
