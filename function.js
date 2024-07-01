59a93a07806cf41b8b1e73d258d228375324ef79
KicauID <Kicauid.official@gmail.com>
2024-07-01 12:03:46 +0700

    TMTMT

--- function.js
+++ function.js
@@ -144,9 +144,25 @@ window.function = function (html, fileName, format, zoom, orientation, margin, b
       border-radius: 4px;
     }
 
-    
-    `;
+    /* Custom styles for thermal printers */
+    .thermal-58mm {
+      width: 154px; /* Fixed width */
+      max-width: 154px; /* Max width */
+      min-height: 1050px; /* Minimum height */
+      overflow-wrap: break-word; /* Ensure words don't overflow */
+      word-wrap: break-word;
+      word-break: break-all; /* Break long words */
+    }
 
+    .thermal-80mm {
+      width: 224px; /* Fixed width */
+      max-width: 224px; /* Max width */
+      min-height: 1050px; /* Minimum height */
+      overflow-wrap: break-word; /* Ensure words don't overflow */
+      word-wrap: break-word;
+      word-break: break-all; /* Break long words */
+    }
+    `;
 
     const originalHTML = `
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.9.2/html2pdf.bundle.min.js"></script>
