a2dc5aeb24d7c67c6a06120884729527d69885dc
KicauID <Kicauid.official@gmail.com>
2024-07-03 09:54:13 +0700

    24

--- function.js
+++ function.js
@@ -20,7 +20,7 @@ window.function = function (html, fileName, format, zoom, orientation, margin, f
 
     // DOCUMENT DIMENSIONS
     const formatDimensions = {
-        tiket: [350, 350],
+        tiket: [350, auto],
         invoice: [350, 500],
     };
 
