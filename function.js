1a7ad3db80e98505d7e5f57336486502c4a73237
KicauID <Kicauid.official@gmail.com>
2024-07-05 08:09:15 +0700

    31

--- function.js
+++ function.js
@@ -53,7 +53,8 @@ window.function = function (html, fileName, format, zoom, orientation, margin, f
         34: [350, 5950],
         35: [350, 6125],
         36: [350, 6300],
-        };
+        invoice: [350, 600],
+    };
 
     const dimensions = customDimensions || formatDimensions[format];
     const finalDimensions = dimensions.map((dimension) => Math.round(dimension / zoom));
@@ -72,145 +73,144 @@ window.function = function (html, fileName, format, zoom, orientation, margin, f
 
     const customCSS = `
     body {
-      margin: 0!important;
+        margin: 0!important;
     }
 
     .button {
-      width: 50%;
-      border-radius: 0;
-      font-size: 14px;
-      font-weight: 600;
-      line-height: 1.5rem;
-      color: #ffffff;
-      border: none;
-      font-family: 'Arial';
-      padding: 0px 12px;
-      height: 32px;
-      text-transform: uppercase;
-      cursor: pointer;
-      box-shadow: 0 0 0 0.5px rgba(0, 0, 0, 0.08), 0 1px 2.5px rgba(0, 0, 0, 0.1);
-      position: fixed;
-      top: 0;
-      z-index: 1000;
+        width: 50%;
+        border-radius: 0;
+        font-size: 14px;
+        font-weight: 600;
+        line-height: 1.5rem;
+        color: #ffffff;
+        border: none;
+        font-family: 'Arial';
+        padding: 0px 12px;
+        height: 32px;
+        text-transform: uppercase;
+        cursor: pointer;
+        box-shadow: 0 0 0 0.5px rgba(0, 0, 0, 0.08), 0 1px 2.5px rgba(0, 0, 0, 0.1);
+        position: fixed;
+        top: 0;
+        z-index: 1000;
     }
 
     button#download {
-      background: #04A535;
-      left: 0;
+        background: #04A535;
+        left: 0;
     }
 
     button#print {
-      background: #0353A7;
-      right: 0;
+        background: #0353A7;
+        right: 0;
     }
 
     button#download:hover, button#print:hover {
-      background: #f5f5f5;
-      color: #000000;
+        background: #f5f5f5;
+        color: #000000;
     }
 
     button#download.downloading, button#print.printing {
-      background: #ffffff;
-      color: #000000;
+        background: #ffffff;
+        color: #000000;
     }
 
     button#download.done, button#print.done {
-      background: #ffffff;
-      color: #000000;
+        background: #ffffff;
+        color: #000000;
     }
 
     ::-webkit-scrollbar {
-      width: 5px;
-      background-color: rgb(0 0 0 / 8%);
+        width: 5px;
+        background-color: rgb(0 0 0 / 8%);
     }
 
     ::-webkit-scrollbar-thumb {
-      background-color: rgb(0 0 0 / 32%);
-      border-radius: 4px;
+        background-color: rgb(0 0 0 / 32%);
+        border-radius: 4px;
     }
-
-   `;
+    `;
 
     const originalHTML = `
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.9.2/html2pdf.bundle.min.js"></script>
     <style>${customCSS}</style>
     <div class="main">
-      <button class="button" id="download">Download</button>
-      <button class="button" id="print">Print</button>
-      <div id="content" class="content thermal-${format}">${html}</div>
+        <button class="button" id="download">Download</button>
+        <button class="button" id="print">Print</button>
+        <div id="content" class="content thermal-${format}">${html}</div>
     </div>
     <script>
-      function adjustDimensions() {
-        var contentElement = document.getElementById('content');
-        var contentHeight = contentElement.scrollHeight;
-        var contentWidth = contentElement.scrollWidth;
-        var adjustedDimensions = [contentWidth, contentHeight];
-        return adjustedDimensions;
-      }
+        function adjustDimensions() {
+            var contentElement = document.getElementById('content');
+            var contentHeight = contentElement.scrollHeight;
+            var contentWidth = contentElement.scrollWidth;
+            var adjustedDimensions = ['${format}' === 'invoice' ? 350 : contentWidth, '${format}' === 'invoice' ? 600 : contentHeight];
+            return adjustedDimensions;
+        }
 
-      document.getElementById('download').addEventListener('click', function() {
-        var element = document.getElementById('content');
-        var button = this;
-        button.innerText = 'DOWNLOADING...';
-        button.className = 'downloading';
+        document.getElementById('download').addEventListener('click', function() {
+            var element = document.getElementById('content');
+            var button = this;
+            button.innerText = 'DOWNLOADING...';
+            button.className = 'downloading';
 
-        var adjustedDimensions = adjustDimensions();
-        var opt = {
-          margin: ${margin},
-          filename: '${fileName}',
-          html2canvas: {
-            useCORS: true,
-            scale: ${quality}
-          },
-          jsPDF: {
-            unit: 'px',
-            orientation: '${orientation}',
-            format: adjustedDimensions,
-            hotfixes: ['px_scaling']
-          }
-        };
-        html2pdf().set(opt).from(element).toPdf().get('pdf').then(function(pdf) {
-          button.innerText = 'DOWNLOAD DONE';
-          button.className = 'done';
-          setTimeout(function() { 
-            button.innerText = 'Download';
-            button.className = ''; 
-          }, 2000);
-        }).save();
-      });
-
-      document.getElementById('print').addEventListener('click', function() {
-        var element = document.getElementById('content');
-        var button = this;
-        button.innerText = 'PRINTING...';
-        button.className = 'printing';
-
-        var adjustedDimensions = adjustDimensions();
-        var opt = {
-          margin: ${margin},
-          filename: '${fileName}',
-          html2canvas: {
-            useCORS: true,
-            scale: ${quality}
-          },
-          jsPDF: {
-            unit: 'px',
-            orientation: '${orientation}',
-            format: adjustedDimensions,
-            hotfixes: ['px_scaling']
-          }
-        };
-        html2pdf().set(opt).from(element).toPdf().get('pdf').then(function(pdf) {
-          pdf.autoPrint();
-          window.open(pdf.output('bloburl'), '_blank');
-          button.innerText = 'PRINT DONE';
-          button.className = 'done';
-          setTimeout(function() { 
-            button.innerText = 'Print';
-            button.className = ''; 
-          }, 2000);
+            var adjustedDimensions = adjustDimensions();
+            var opt = {
+                margin: ${margin},
+                filename: '${fileName}',
+                html2canvas: {
+                    useCORS: true,
+                    scale: ${quality}
+                },
+                jsPDF: {
+                    unit: 'px',
+                    orientation: '${orientation}',
+                    format: adjustedDimensions,
+                    hotfixes: ['px_scaling']
+                }
+            };
+            html2pdf().set(opt).from(element).toPdf().get('pdf').then(function(pdf) {
+                button.innerText = 'DOWNLOAD DONE';
+                button.className = 'done';
+                setTimeout(function() { 
+                    button.innerText = 'Download';
+                    button.className = ''; 
+                }, 2000);
+            }).save();
         });
-      });
+
+        document.getElementById('print').addEventListener('click', function() {
+            var element = document.getElementById('content');
+            var button = this;
+            button.innerText = 'PRINTING...';
+            button.className = 'printing';
+
+            var adjustedDimensions = adjustDimensions();
+            var opt = {
+                margin: ${margin},
+                filename: '${fileName}',
+                html2canvas: {
+                    useCORS: true,
+                    scale: ${quality}
+                },
+                jsPDF: {
+                    unit: 'px',
+                    orientation: '${orientation}',
+                    format: adjustedDimensions,
+                    hotfixes: ['px_scaling']
+                }
+            };
+            html2pdf().set(opt).from(element).toPdf().get('pdf').then(function(pdf) {
+                pdf.autoPrint();
+                window.open(pdf.output('bloburl'), '_blank');
+                button.innerText = 'PRINT DONE';
+                button.className = 'done';
+                setTimeout(function() { 
+                    button.innerText = 'Print';
+                    button.className = ''; 
+                }, 2000);
+            });
+        });
     </script>
     `;
     var encodedHtml = encodeURIComponent(originalHTML);
