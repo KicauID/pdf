b9f3d8862c1870539a549e28783edddbf61a1ca4
KicauID <Kicauid.official@gmail.com>
2024-10-09 11:51:04 +0700

    Update Sertifikat A4

--- function.js
+++ function.js
@@ -55,6 +55,7 @@ window.function = function (html, fileName, format, zoom, orientation, margin, f
         35: [350, 6125],
         36: [350, 6300],
         invoice: [350, 650],
+        A4: [794, 1123]
     };
 
     const dimensions = customDimensions || formatDimensions[format];
